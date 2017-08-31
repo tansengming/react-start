c9407daecd1a380dff7cb92f5dedb85e8eea3adf
SengMing Tan <tansengming@gmail.com>
2017-08-31 11:18:08 -0400

    initial commit of temp calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import Clock from './Clock.js'
-import LoginControl from './LoginControl.js'
+// import LoginControl from './LoginControl.js'
+import TempCalculator from './TempCalculator.js'
 import './App.css';
 
 class App extends Component {
   render() {
-    return (<LoginControl />)
+    return (<TempCalculator />)
   }
 }
 
